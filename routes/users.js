aeed51a5218c1f8b6e58719fa088c77d797d369d
DexteR-mask <jaspreet3548.be20@chitkara.edu.in>
2023-03-14 12:09:27 +0530

    added JWT and get tweet function

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const bcrypt = require('bcrypt');
-const Joi = require('joi');
-var { user, tweet } = require('../mongose')
-/* GET users listing. */
-
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+var jwt = require("jsonwebtoken");
+var { user, tweet } = require("../mongose");
+const { boolean } = require("joi");
+const secret = "jkvikjhbkj";
 const Checkjoi = Joi.object({
   username: Joi.string().token(),
   password: Joi.string().min(8),
   age: Joi.number(),
-  name: Joi.string().pattern(/^[a-zA-Z]+$/)
+  name: Joi.string().pattern(/^[a-zA-Z]+$/),
 });
 
-router.use()
+//------------------------------------------
 
 function getU(res) {
   user.find({}).then(function (users) {
@@ -20,101 +21,122 @@ function getU(res) {
   });
 }
 
-
-router.get('/', function (req, res, next) {
-  getU(res)
+//------------------------------------------
+router.get("/", function (req, res, next) {
+  getU(res);
 });
+//------------------------------------------
 
+router.post("/tweet", function (req, res) {
+  tweet.find({ _id: req.body.id }).then(function (tweet) {
+    res.status(200).send(tweet);
+  });
+});
+//------------------------------------------
 
-
-router.post('/', function (req, res) {
-  res.status(400);
-})
-
-
-
-
-
-router.post('/add', async function (req, res) {
+router.post("/add", async function (req, res) {
   const hash = bcrypt.hashSync(req.body.password, 10);
 
-  const ret = Checkjoi.validate(
-    {
+  const ret = Checkjoi.validate({
+    username: req.body.username,
+    password: hash,
+    age: req.body.age,
+    name: req.body.name,
+  });
+  if (ret.error != undefined) {
+    res.status(400).json(ret.error);
+  } else {
+    const doc1 = new user({
       username: req.body.username,
       password: hash,
       age: req.body.age,
-      name: req.body.name
-    }
-  )
-  if (ret.error != undefined) {
-    res.status(400).json(ret.error);
-  }
-  else {
-    const doc1 = new user
-      (
-        {
-          username: req.body.username,
-          password: hash,
-          age: req.body.age,
-          name: req.body.name
-        }
-      )
+      name: req.body.name,
+    });
     // await user.deleteMany({});
     await doc1.save();
-
     getU(res);
   }
+});
+//------------------------------------------
 
-})
+router.post("/login", function (req, res) {
+  user.findOne({ username: req.body.username }).then(function (user) {
+    if (user === null) {
+      res.status(400).send("user Not Found");
+    } else {
+      bcrypt.compare(req.body.password, user.password).then(function (result) {
+        if (result == true) {
+          var token = jwt.sign(
+            {
+              data: user.username,
+            },
+            secret,
+            { expiresIn: 60 * 5 }
+          );
+          res.status(200).send(token);
+        } else {
+          res.status(400).send("password Incoreect");
+        }
+      });
+    }
+  });
+});
 
+//------------------------------------------
 
+router.post("/verify", function (req, res) {
+  jwt.verify(req.body.token, secret, function (err, decoded) {
+    if (err == null) {
+      res.status(200).send(decoded.data); // bar
+    } else {
+      res.status(400).send(err);
+    }
+  });
+});
 
+//------------------------------------------
 
-
-
-
-router.post('/addtweet', async function (req, res) {
-
-  const tweet1 = new tweet
-    (
-      {
-        msg: req.body.msg
-      }
-    )
+router.post("/addtweet", async function (req, res) {
+  const tweet1 = new tweet({
+    msg: req.body.msg,
+  });
   tweet1.save();
-  await tweet.updateOne({ username: req.body.username }, {
-    $push: { tweets: tweet1 }
-  });
+  await tweet.updateOne(
+    { username: req.body.username },
 
-  getU(res)
+    {
+      $push: { tweets: tweet1 },
+    }
+  );
+  getU(res);
+});
 
-})
+//------------------------------------------
 
-router.post('/editweet', async function (req, res) {
+router.post("/editweet", async function (req, res) {
+  await user.updateOne(
+    { _id: req.body.id },
+    {
+      msg: req.body.msg,
+    }
+  );
+  getU(res);
+});
 
-  
-  await user.updateOne({ _id : req.body.id }, {
-    msg : req.body.msg
-  });
+//------------------------------------------
 
-  getU(res)
-
-})
-
-
-
-
-
-router.post('/remove', async function (req, res) {
-
+router.post("/remove", async function (req, res) {
   await user.deleteOne({ username: req.body.username });
   // doc1.save();
-  getU(res)
+  getU(res);
+});
 
-})
+//------------------------------------------
 
+router.all("*", function (req, res) {
+  res.status(404).send("not found");
+});
 
-
-
+//------------------------------------------
 
 module.exports = router;
